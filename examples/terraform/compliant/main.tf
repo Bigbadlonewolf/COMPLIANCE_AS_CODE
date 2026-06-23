43fb4780efed1b223f2c54c714cc754a699a1a1f
LANRE OLUOKUN <Lanreolu88@yahoo.com>
2026-06-22 21:28:32 -0400

    Fix missing settings block on orders_db, add network data source reference

--- examples/terraform/compliant/main.tf
+++ examples/terraform/compliant/main.tf
@@ -65,12 +65,30 @@ resource "google_vpc_access_connector" "payment_connector" {
   }
 }
 
+data "google_compute_network" "default" {
+  name = "default"
+}
+
 resource "google_sql_database_instance" "orders_db" {
   name             = "orders-db"
   database_version = "POSTGRES_15"
   region           = "us-central1"
 
   encryption_key_name = "projects/securecart-prod/locations/us-central1/keyRings/cde/cryptoKeys/orders-db-key"
+
+  settings {
+    tier              = "db-custom-2-8192"
+    availability_type = "REGIONAL"
+
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = data.google_compute_network.default.id
+    }
+
+    backup_configuration {
+      enabled = true
+    }
+  }
 }
 
 resource "google_project_iam_member" "contractor_scoped_access" {
